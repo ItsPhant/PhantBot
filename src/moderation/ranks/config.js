6688de89fbc76ead7e8eb5ea455a03b8d1d6c9f4
Ellie Phant <itsphant@gmail.com>
2018-03-21 18:19:08 -0400

    Update header

--- src/moderation/ranks/config.js
+++ src/moderation/ranks/config.js
@@ -1,13 +1,13 @@
 /**
- * Dommebot is a free multipurpose bot made for Discord.
+ * PhantBot is a free multipurpose bot made for Discord.
  * Copyright (C) 2018  Ellie Phant
  *
- * Dommebot is free software: you can redistribute it and/or modify
+ * PhantBot is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * Dommebot is distributed in the hope that it will be useful,
+ * PhantBot is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
